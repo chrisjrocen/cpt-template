40350b736b507e1eead592598dbc5540db538dd2
chrisocen <105217816+chrisocen@users.noreply.github.com>
2022-11-09 20:04:34 +0300

    added more labels

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cpt-template
 A Template for creating a Custom Post Type in WordPress
+
+The following example registers a new post type, Products, which is identified in the database as cpt_product.
+
+**Warning:** You must call ```Php register_post_type()``` before the ```Php admin_init``` hook and after the ```Php after_setup_theme``` hook. A good hook to use is the ```Php init``` action hook.
+
+## Naming Best Practices
+
+Always prefix your post type functions and identifiers with a short prefix that corresponds to your plugin, theme, or website. If your identifier is too generic (for example: “product“), it may conflict with other plugins or themes that have chosen to use that same identifier.
+
+**Note:** Solving duplicate post type identifiers is not possible without disabling one of the conflicting post types.
+
+
+**Warning:** 
+
+1. Your custom post type identifier should not exceed 20 characters
+1. To ensure forward compatibility, do not use wp_ as your identifier — it is being used by WordPress core.
+
--- cpt-template.php
+++ cpt-template.php
@@ -8,8 +8,8 @@
 add_action( 'init', 'register_products_post_type' );
 function register_products_post_type() { 
 	$labels = array(
-		'name'               => __( 'Products', 'text-domain' ),
-		'singular_name'      => __( 'Product', 'text-domain' ),
+		'name'               => __( 'Products', 'Post type general name', 'text-domain' ),
+		'singular_name'      => __( 'Product', 'Post type singular name', 'text-domain' ),
 		'add_new'            => __( 'Add New', 'text-domain' ),
 		'add_new_item'       => __( 'Add New Product', 'text-domain' ),
 		'edit_item'          => __( 'Edit Product', 'text-domain' ),
@@ -18,6 +18,21 @@ function register_products_post_type() {
 		'search_items'       => __( 'Search Products', 'text-domain' ),
 		'not_found'          => __( 'No Product Found', 'text-domain' ),
 		'not_found_in_trash' => __( 'No Product found in Trash', 'text-domain' ),
+		'menu_name'             => __( 'Products', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar'        => __( 'Product', 'Add New on Toolbar', 'textdomain' ),
+		'all_items'             => __( 'All Products', 'textdomain' ),
+		'parent_item_colon'     => __( 'Parent Products:', 'textdomain' ),
+		//Most of the options below are optional.Use them only when you need them
+		'featured_image'        => __( 'Product Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'set_featured_image'    => __( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'remove_featured_image' => __( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'use_featured_image'    => __( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'archives'              => __( 'Product archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+		'insert_into_item'      => __( 'Insert into product', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this product', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+		'filter_items_list'     => __( 'Filter products list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+		'items_list_navigation' => __( 'Products list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+		'items_list'            => __( 'Products list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
 	);
 	$args = array(
 		'label'               => __( 'Products', 'text-domain' ),
@@ -45,5 +60,5 @@ function register_products_post_type() {
 		),
 
 	);
-	register_post_type( 'products', $args );
+	register_post_type( 'cpt_products', $args );
 }
